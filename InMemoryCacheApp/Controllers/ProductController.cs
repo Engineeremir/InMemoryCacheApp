0534012f6ce5629caedc7e012fb79281fed27989
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-10-15 15:01:39 +0300

    Add project files.

---
+++ InMemoryCacheApp/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCacheApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+                _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time", DateTime.UtcNow.ToString());
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.time = _memoryCache.Get<string>("time");
+            return View();
+        }
+    }
+}
---
+++ InMemoryCacheApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InMemoryCacheApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
