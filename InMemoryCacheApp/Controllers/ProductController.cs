c37fcbf7700ba7be346e212db7077838def6d522
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-10-15 15:35:42 +0300

    RegisterPostEvictionCallback method

--- InMemoryCacheApp/Controllers/ProductController.cs
+++ InMemoryCacheApp/Controllers/ProductController.cs
@@ -15,18 +15,24 @@ namespace InMemoryCacheApp.Controllers
             
             MemoryCacheEntryOptions memoryCacheEntryOptions = new MemoryCacheEntryOptions();
             memoryCacheEntryOptions.AbsoluteExpiration = DateTime.UtcNow.AddSeconds(10);
-            memoryCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(10);
+            //memoryCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(10);
             memoryCacheEntryOptions.Priority = CacheItemPriority.High;
+            memoryCacheEntryOptions.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}=>{value}=>reason:{reason}");
+            });
             _memoryCache.Set<string>("time", DateTime.UtcNow.ToString(),memoryCacheEntryOptions);
             
             return View();
         }
         public IActionResult Show()
         {
+            _memoryCache.TryGetValue("callback", out string callback);
             _memoryCache.TryGetValue("time", out string timeCache);
 
 
             ViewBag.time = timeCache;
+            ViewBag.callback = callback;
             return View();
         }
     }
